64dacdd6461a45fe58f9e86695fab3adfb2d7fe9
Rémi BOUILLET <rbouillet@r01p09.kb2.kb.sm.prologin.org>
2022-05-28 11:46:05 +0200

    Eurostar

--- Champion.py
+++ Champion.py
@@ -30,6 +30,16 @@ def caseLibre(pos):
                     return False
     return True
 
+def getTrous():
+    rep = []
+    for y in range(HAUTEUR):
+        for x in range(LARGEUR):
+            pos = (x,y,0)
+            if info_case(pos).contenu == type_case.TROU:
+                rep.append(pos)
+    return rep
+
+
 def getPtsActions(numTroupe):
     return troupes_joueur(moi())[numTroupe].pts_action
     
@@ -50,6 +60,15 @@ def printMap():
     for y in range(HAUTEUR):
         for x in range(LARGEUR):
             print(info_case((x,y,0)).contenu, end="")
+        print()
+
+def printTunnels():
+    for y in range(HAUTEUR):
+        for x in range(LARGEUR):
+            if info_case((x,y,-1)).contenu == type_case.TUNNEL:
+                print("#", end="")
+            else:
+                print(" ", end="")
         print()
 
 def getNidsJoueur(joueur, ouLibre):
@@ -175,7 +194,7 @@ def grandirEtAvancer(numTroupe):
 def consommerPtsActions(numTroupe):
     troupe = troupes_joueur(moi())[numTroupe]
 
-    for _ in range(10000):
+    for _ in range(HAUTEUR*LARGEUR):
         x = random.randrange(LARGEUR)
         y = random.randrange(HAUTEUR)
         path = trouver_chemin(troupe.maman, (x, y, 0))
@@ -191,15 +210,51 @@ def consommerPtsActions(numTroupe):
             consommerPtsActions(numTroupe)
             return
 
+def getScorePos(pos):
+    score = 0
+    for papy in papys:
+        if pos == papy:
+            score += 3
+        d = len(trouver_chemin(pos, papy))
+        if d > 0:
+            score += 1/d
+    return score
+
+def getBestScore(positions):
+    maxi = getScorePos(positions[0])
+    best = positions[0]
+    for pos in positions[1:]:
+        s = getScorePos(pos)
+        if s > maxi:
+            maxi = s
+            best = pos
+    return best
+
 papys = []
+aCreuser = []
 # Fonction appelée au début de la partie.
 def partie_init():
-    global papys
+    global papys, aCreuser
     for y in range(HAUTEUR):
         for x in range(LARGEUR):
             pos = (x, y, 0)
             if info_case(pos).contenu == type_case.PAPY:
                 papys.append(pos)
+
+
+    trous = getTrous()
+    for troupe in troupes_joueur(moi()):
+        if len(trous) > 1:
+            departTunnel = getClosest(troupe.maman, trous)
+            trous.remove(departTunnel)
+            arriveeTunnel = getBestScore(trous)
+
+            for x in range(min(departTunnel[0],arriveeTunnel[0]), max(departTunnel[0],arriveeTunnel[0])+1):
+                aCreuser.append((x, departTunnel[1], -1))
+            for y in range(min(departTunnel[1],arriveeTunnel[1]), max(departTunnel[1],arriveeTunnel[1])+1):
+                aCreuser.append((x, y, -1))
+    aCreuser = list(set(aCreuser))
+    trace(aCreuser)
 
 
 TOUR = 0
@@ -208,6 +263,13 @@ trolling = 0
 def jouer_tour():
     global TOUR, trolling
     trace("================================ TOUR", TOUR, "================================")
+    for _ in range(FREQ_TUNNEL):
+        if len(aCreuser) > 0:
+            r = creuser_tunnel(aCreuser.pop())
+            if r != erreur.OK:
+                    afficher_erreur(r)
+    if DEBUG:
+        printTunnels()
     for numTroupe, troupe in enumerate(troupes_joueur(moi())):
         if moi() == 0:
             debug_poser_pigeon(troupe.maman, pigeon_debug.PIGEON_ROUGE)
